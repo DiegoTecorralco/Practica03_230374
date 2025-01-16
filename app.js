4185d6543e5764960e8979b063877a6f540bf8e3
Teco <230374@utxicotepec.edu.mx>
2025-01-16 10:10:52 -0600

    Practica 03 corrección

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const session = require('express-session');
+const moment = require('moment-timezone')
 
 const app = express();
 
 // Configuración de la sesión
 app.use(session({
-    secret: 'mi-clave-secreta', // Secreto para firmar la cookie de sesión
+    secret: 'p3-DSTM-#aby-sesionespersistentes', // Secreto para firmar la cookie de sesión
     resave: false, // No resguardar la sesión si no ha sido modificada
     saveUninitialized: false, // Guardar la sesión aunque no haya sido inicializada
-    cookie: { secure: false } // Usar secure:true solo si usas HTTPS
+    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 }  // maxAge para los milisegundos
 }));
 
-// Middleware para inicializar y actualizar tiempos en la sesión
+/*// Middleware para inicializar y actualizar tiempos en la sesión
 app.use((req, res, next) => {
     if (req.session) {
         if (!req.session.createdAt) {
@@ -20,14 +21,57 @@ app.use((req, res, next) => {
         req.session.lastAccess = Date.now(); // Actualizar el último acceso en milisegundos
     }
     next();
-});
+});*/
 
-app.get('/login/:User',(req,res)=>{
-    req.session.User=req.params.User;
-    res.send("Usuario guardado");
+//Ruta para inicializar la sesion
+app.get('/login/',(req,res)=>{
+    if(!req.session.createdAt){
+        req.session.createdAt = new Date();
+        req.session.lastAccess = new Date();
+        res.send("La sesión ha sido iniciada exitosamente");
+    }else{
+        res.send("Ya existe una sesión ");
+    }
 })
 
-// Ruta para mostrar la información de la sesión
+//Ruta oara actualizar la decha de última consulta
+app.get('/update',(req,res)=>{
+    if(!req.session.createdAt){
+        req.session.lastAccess = new Date();
+        res.send("La fecha de último acceso ha sido actualizada exitosamente");
+    }else{
+        res.send("No hay una sesión activa");
+    }
+})
+//Ruta para obtener el estado de la sesión
+app.get('/status',(req,res)=>{
+    if(!req.session.createdAt){
+        const now = new Date();
+        const started = new Date(req.session.createdAt);
+        const lastUpdate = new Date(req.session.lastAccess);
+
+//Calcular la antiguedad de la sesión
+const sessionAgeMs= now - started;
+        const hours = Math.floor(sessionAgeMs / (1000 * 60 * 60));
+        const minutes = Math.floor((sessionAgeMs % (1000 * 60 * 60)) / (1000 *60 ));
+        const seconds = Math.floor((sessionAgeMs % (1000 * 60)) / 1000);
+
+    // Convertir las fechas al uso horario de CDMX
+    const createdAt_CDMX = moment(started).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss');
+    const lastAccess_CDMX = moment(started).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss');
+    res.json({
+        mensaje: 'Estado de la sesión',
+        sesionID: req.sessionID,
+        inicio: createdAt_CDMX,
+        ultimoAcceso: lastAccess_CDMX,
+        antiguedas: `${hours} horas, ${minutes} minutos, ${seconds} segundos`
+    });
+    }else{
+        res.send("No hay una sesión activa");
+    }
+})
+
+/*// Ruta para mostrar la información de la sesión
 app.get('/session', (req, res) => {
     if (req.session) {
         const User = req.session.User;
@@ -45,16 +89,20 @@ app.get('/session', (req, res) => {
             <p><strong>Duración de la sesión (en segundos):</strong> ${sessionDuration}</p>
         `);
     }
-});
+});*/
 
 // Ruta para cerrar la sesión
 app.get('/logout', (req, res) => {
+    if(req.session.createdAt){
     req.session.destroy((err) => {
         if (err) {
-            return res.send('Error al cerrar sesión.');
+            return res.status(500).send('Error al cerrar sesión.');
         }
         res.send('<h1>Sesión cerrada exitosamente.</h1>');
     });
+    }else{
+        res.send("No hay una sesión activa");
+    }
 });
 
 // Iniciar el servidor en el puerto 3000
